da6f0bcdc08572502f9ed71a17397d394b0dc7ef
maneereddy <85781395+maneereddy@users.noreply.github.com>
2021-06-15 09:24:36 +0530

    Add files via upload

---
+++ day4 challenge.py
@@ -0,0 +1,24 @@
+
+
+#day4exercise
+#1)
+a,b,c = 10,20,30
+print(a/10,b*50,c+60)
+
+#2)
+a = 'manee'
+
+
+print("string is immutable")
+
+#3)
+a,b=3,2.4
+print(float(a))
+print(int(b))
+
+
+
+
+
+
+
