8af364637aacc9220efc645237df0b4dcce497e6
maneereddy <85781395+maneereddy@users.noreply.github.com>
2021-06-18 10:01:59 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,17 @@
+#1)
+def function(a,b):
+    print("addition of a,b:",a+b)
+    print("subtraction of a,b:",a-b)
+    print("division of a,b:",a/b)
+    print("multiplication of a,b:",a*b)
+
+a = int(input("enter first number"))
+b = int(input("enter second number"))
+function(a,b)
+
+#2)
+def covid(name,temperature=98):
+     print("patient name:",name)
+     print("body temperature:",temperature)
+
+covid("manee")
