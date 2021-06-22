5d7bc7586e5826a22521f23750b90f3f948e5ac0
manee <85781395+maneereddy@users.noreply.github.com>
2021-06-22 11:14:16 +0530

    Update day10.py

--- day10.py
+++ day10.py
@@ -1,3 +1,4 @@
+#banking concept
 class account:
     def __init__(self,name,address):
         self.name = name
@@ -14,3 +15,19 @@ class bank(account):
 a = bank("manee","hyd",1234)
 a.welcome()
 
+#Ecommerce concept
+class account:
+    def __init__(self,name,address):
+        self.name = name
+        self.address = address
+    def printname(self):
+         print(self.name,self.address)
+class flipcart(account):
+    def __init__(self,name,address,acnumber):
+     super().__init__(name,address)
+     self.number = acnumber
+    def welcome(self):
+        print("welcome",self.name,"to your order id is ",self.number)
+
+a = flipcart("ram","chennai","ID321")
+a.welcome()
