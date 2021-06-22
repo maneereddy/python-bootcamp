74634c69404fd9c490ebb8ca4d39dbe1707ed9e4
manee <85781395+maneereddy@users.noreply.github.com>
2021-06-22 11:09:56 +0530

    Add files via upload

---
+++ day10.py
@@ -0,0 +1,16 @@
+class account:
+    def __init__(self,name,address):
+        self.name = name
+        self.address = address
+    def printname(self):
+         print(self.name,self.address)
+class bank(account):
+    def __init__(self,name,address,acnumber):
+     super().__init__(name,address)
+     self.number = acnumber
+    def welcome(self):
+        print("welcome",self.name,"to your account number",self.number)
+
+a = bank("manee","hyd",1234)
+a.welcome()
+
