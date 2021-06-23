32e78e4ef0cf5b419b3732a1379da17aa6b6c21d
manee <85781395+maneereddy@users.noreply.github.com>
2021-06-23 11:04:04 +0530

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,27 @@
+#1)
+#importing the fun module
+import fun
+
+fun.function()
+#modifying the list
+print(fun.lst)
+
+#2)
+#importing pandas library
+import pandas as pd
+
+#3)
+import random
+for i in range(1,101):
+  print(random.randint(i,i+1))
+
+#4)
+import sys
+print(sys.path)
+#5)
+
+
+
+
+
+
